b4a1aff5a2a375ddb2dee01e7f2dca117428f893
Aitrsle <aitor.sole@gracia.lasalle.cat>
2026-02-06 12:41:03 +0100

    fix : Arreglar Clase mago

--- Mago.java
+++ Mago.java
@@ -1,26 +1,46 @@
 package Videojoc_Programacio;
+
 public class Mago extends Entidad {
+
     private int mana;
     private int daño;
+
+    public Mago(String nombre, int nivel, int vida, int mana) {
+        super(nombre, nivel, vida);
+        this.mana = mana;
+    }
 
     public int getMana() {
         return mana;
     }
+
     public void setMana(int mana) {
-        this.mana = mana;
+        if (mana < 0) this.mana = 0;
+        else this.mana = mana;
     }
 
     public int getDaño() {
         return daño;
     }
-    public void setDaño(int daño) {
-        this.daño = daño;
-    }
 
     @Override
-    public void Atacar() {
-        setDaño(1);
-        setMana(getMana() - 1);
-        System.out.println(getNombre() + " llança magia // Dany: " + getDaño());
+    public void atacar() {
+        if (mana > 0) {
+            daño = 1;
+            mana--;
+            System.out.println(getNombre() + " Lanza encantamiento // Danyo: " + daño);
+        } else {
+            System.out.println(getNombre() + " No tiene mana!");
+        }
+    }
+
+    // CARTA ESPECIAL
+    public void bolaDeFoc() {
+        if (mana >= 3) {
+            mana -= 3;
+            System.out.println(getNombre() + " Lanza BOLA DE FUEGO!");
+        } else {
+            System.out.println(getNombre() + " No tiene mana suficiente!");
+        }
     }
 }
