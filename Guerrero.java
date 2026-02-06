39bc6ab3475068da585c19a764c68cb25500ec3a
Aitrsle <aitor.sole@gracia.lasalle.cat>
2026-02-06 12:39:56 +0100

    fix : Arreglar la clase de guerrero

--- Guerrero.java
+++ Guerrero.java
@@ -1,11 +1,18 @@
 package Videojoc_Programacio;
+
 public class Guerrero extends Entidad {
     private int resistencia;
     private int daño;
 
+    public Guerrero(String nombre, int nivel, int vida, int resistencia) {
+        super(nombre, nivel, vida);
+        this.resistencia = resistencia;
+    }
+
     public int getResistencia() {
         return resistencia;
     }
+
     public void setResistencia(int resistencia) {
         this.resistencia = resistencia;
     }
@@ -13,14 +20,11 @@ public class Guerrero extends Entidad {
     public int getDaño() {
         return daño;
     }
-    public void setDaño(int daño) {
-        this.daño = daño;
-    }
 
     @Override
-    public void Atacar() {
-        setDaño(2);
-        System.out.println(getNombre() + " ataca físicament // Dany: " + getDaño());
+    public void atacar() {
+        daño = 2;
+        System.out.println(getNombre() + " ataca con fuerza física // Danyo: " + daño);
     }
 
     @Override
@@ -28,5 +32,10 @@ public class Guerrero extends Entidad {
         int danyFinal = quantitat - resistencia;
         if (danyFinal < 0) danyFinal = 0;
         setVida(getVida() - danyFinal);
+    }
+
+    // CARTA ESPECIAL
+    public void copEscut() {
+        System.out.println(getNombre() + " usa Golpe de Escudo!");
     }
 }
