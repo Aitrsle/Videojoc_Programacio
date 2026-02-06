6f14b66faf11cd6b6801295a52dcaf1c64fae0a0
Aitrsle <aitor.sole@gracia.lasalle.cat>
2026-02-06 12:43:08 +0100

    fix :  Arreglar Clase padre

--- Entidad.java
+++ Entidad.java
@@ -1,40 +1,52 @@
 package Videojoc_Programacio;
+
 public abstract class Entidad {
+
     private String nombre;
     private int nivel;
     private int vida;
 
-    public Entidad (String nombre, int nivel, int vida) {
+    public Entidad(String nombre, int nivel, int vida) {
         this.nombre = nombre;
         this.nivel = nivel;
-        this.vida = vida;
+        setVida(vida);
     }
-    public String getNombre() { 
-        return nombre; 
-    }
-    public void setNombre(String nombre) { 
-        this.nombre = nombre;
-     }
 
-    public int getNivel() { 
-        return nivel; 
+    public String getNombre() {
+        return nombre;
     }
-    public void setNivel(int n) {
-        if (n == this.nivel + 1) this.nivel = n;
-        else System.out.println("ACCÉS DENEGAT");
+
+    public int getNivel() {
+        return nivel;
     }
 
     public int getVida() {
-         return vida;
-         }
+        return vida;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setNivel(int n) {
+        if (n == this.nivel + 1) {
+            this.nivel = n;
+        } else {
+            System.out.println("ACCESO DENEGADO: Solo puedes subir un nivel a la vez.");
+        }
+    }
+
     public void setVida(int v) {
-        if (v < 0) this.vida = 0;
-        else this.vida = v;
+        if (v < 0) {
+            this.vida = 0;
+        } else {
+            this.vida = v;
+        }
     }
 
     public void rebreDany(int quantitat) {
         setVida(this.vida - quantitat);
     }
 
-    public abstract void Atacar();
+    public abstract void atacar();
 }
