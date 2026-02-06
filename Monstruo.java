47313cbf0886f0b64b68b1f9ef090d10d2a175db
Aitrsle <aitor.sole@gracia.lasalle.cat>
2026-02-06 12:42:03 +0100

    fix : Cambiar clase Monstruo

--- Monstruo.java
+++ Monstruo.java
@@ -1,17 +1,25 @@
 package Videojoc_Programacio;
+
 public class Monstruo extends Entidad {
+
     private int daño;
+
+    public Monstruo(String nombre, int nivel, int vida) {
+        super(nombre, nivel, vida);
+    }
 
     public int getDaño() {
         return daño;
     }
-    public void setDaño(int daño) {
-        this.daño = daño;
-    }
 
     @Override
-    public void Atacar() {
-        setDaño(3);
-        System.out.println(getNombre() + " ataca salvatge // Dany: " + getDaño());
+    public void atacar() {
+        daño = 3;
+        System.out.println(getNombre() + " Ataca de forma salvaje // Danyo: " + daño);
+    }
+
+    // CARTA 
+    public void rugido() {
+        System.out.println(getNombre() + " Hace Rugido!");
     }
 }
