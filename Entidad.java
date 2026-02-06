bce2f5343a47662bbcc4dd262a13a3d6bed982e3
Aitrsle <aitor.sole@gracia.lasalle.cat>
2026-02-06 09:56:43 +0100

    feat : Crear clase Entidad y modificar Main

---
+++ Entidad.java
@@ -0,0 +1,27 @@
+package Videojoc_Programacio;
+public abstract class Entidad {
+    private String nombre;
+    private int nivel;
+    private int vida;
+
+    public String getNombre() { return nombre; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+
+    public int getNivel() { return nivel; }
+    public void setNivel(int n) {
+        if (n == this.nivel + 1) this.nivel = n;
+        else System.out.println("ACCÉS DENEGAT");
+    }
+
+    public int getVida() { return vida; }
+    public void setVida(int v) {
+        if (v < 0) this.vida = 0;
+        else this.vida = v;
+    }
+
+    public void rebreDany(int quantitat) {
+        setVida(this.vida - quantitat);
+    }
+
+    public abstract void Atacar();
+}
--- Main.java
+++ Main.java
@@ -2,5 +2,8 @@ package Videojoc_Programacio;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.printf("Bevingut al joc de classes!");
+
+
     }
-    }
+}
